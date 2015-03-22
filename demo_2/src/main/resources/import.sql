4a8ba277aab060e29e082e7638476bdee16e4543
Jiangming Yang <yangjm@gmail.com>
2015-03-21 21:14:14 -0400

    use English in DB to avoid import.sql encoding problem

--- demo_2/src/main/resources/import.sql
+++ demo_2/src/main/resources/import.sql
@@ -1,14 +1,14 @@
 -- 权限组
-insert into SYS_RIGHT_CATS(RIGHT_CAT_ID, NAME, CAT_CODE) values(1, '系统权限', 'sys')
-insert into SYS_RIGHT_CATS(RIGHT_CAT_ID, NAME, CAT_CODE) values(2, '帮助权限', 'help')
+insert into SYS_RIGHT_CATS(RIGHT_CAT_ID, NAME, CAT_CODE) values(1, 'System', 'sys')
+insert into SYS_RIGHT_CATS(RIGHT_CAT_ID, NAME, CAT_CODE) values(2, 'Help', 'help')
 
 -- 权限
-insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(1, 1, '权限组管理', 'rightcat', '维护系统权限组')
-insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(2, 1, '权限管理', 'right', '管理权限组中的权限')
-insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(3, 1, '用户管理', 'user', '用户账号管理')
+insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(1, 1, 'Right Category', 'rightcat', 'User right category management')
+insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(2, 1, 'Right', 'right', 'User right management')
+insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(3, 1, 'User', 'user', 'User account management')
 
-insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(4, 2, '演示介绍', 'intro', '简单介绍本演示')
-insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(5, 2, '问题解答', 'qa', '常见问题解答')
+insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(4, 2, 'Introduction', 'intro', 'Introduction of this demo')
+insert into SYS_RIGHTS(RIGHT_ID, RIGHT_CAT_ID, NAME, RIGHT_CODE, DESCRIPTION) values(5, 2, 'Q & A', 'qa', 'Answer of common questions')
 
 -- 用户
 insert into SYS_USERS(USER_ID, USER_NAME, LOGIN_NAME, PASSWORD, EMAIL) values(1, 'Administrator', 'admin', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 'demo@aggrepoint.com')
